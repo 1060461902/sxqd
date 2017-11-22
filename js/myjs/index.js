2829459a260207bce2c447e3b02af82d9ad597f2
Zershaw <836613736@qq.com>
2017-11-22 17:55:08 +0800

    save

---
+++ js/myjs/index.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){ 
+    if ($(".badge").text()==0) {
+      $(".badge").css("display","none")
+    };
+  $("[href='data_analysis.html']").click(function(){
+    $(".nav-title").text("数据分析");
+    if ($(".badge").text()==0) {
+      $(".badge").css("display","none")
+    };
+  });
+  $("[href='approval.html']").click(function(){
+    $(".nav-title").text("审批");
+    if ($(".badge").text()==0) {
+      $(".badge").css("display","none")
+    };
+  });
+  $("[href='btn_table.html']").click(function(){
+    $(".nav-title").text("信息列表");
+    if ($(".badge").text()==0) {
+      $(".badge").css("display","none")
+    };
+  }); 
+  $("[href='set.html']").click(function(){
+    if ($(".badge").text()==0) {
+      $(".badge").css("display","none")
+    };
+    $(".nav-title").text("设置");
+  });   
+
+      $(".loginout").click(function(){
+            $.getJSON("./js/json/validate1.json", function(data) {
+                    if (data.code == "200") {
+                         location.href="admin-login.html"
+                    }
+                    else if(data.code == "500"){
+                         alert('请再次尝试');
+                    }
+                    else{
+                         alert("服务器异常");
+                    }
+                })
+    });
+});
