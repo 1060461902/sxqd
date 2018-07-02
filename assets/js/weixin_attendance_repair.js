2846735db684f0aa692a962cf65855e32b08d1d4
xjc <ccyxxjc@126.com>
2018-07-02 22:42:07 +0800

    7-2

--- assets/js/weixin_attendance_repair.js
+++ assets/js/weixin_attendance_repair.js
@@ -8,6 +8,10 @@ $(document).ready(function () {
      */
     var now = new Date();
     $('#user-select-time').html(now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate());
+
+    //模拟插点
+    addpoint(1,'2018-6-1');
+    addpoint(1,'2018-6-2');
 });
 
 /**
@@ -19,3 +23,18 @@ $(document).on('click', '.picker-calendar-day', function () {
     dateArray[1] = Number(dateArray[1]) + 1;
     $('#user-select-time').html(dateArray.join('-'));
 })
+
+/**
+ * 
+ */
+function addpoint(status, str) {
+    if (status != 0) { //默认状态.改
+        var point_url = '';
+        switch (status) {
+            case 1:
+                point_url = "./assets/images/point_normal.png";
+                break;
+        }
+        $("[data-date='" + str + "']").append("<img class='info-point' src='"+point_url+"' alt=''/>");
+    }
+}
