99df12adc132be5ff2ee1f89b1bc224c626ebbdc
name <s>
2018-07-03 19:56:41 +0800

    7-3

--- assets/js/weixin_attendance_index.js
+++ assets/js/weixin_attendance_index.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
     /**
+     * 
+     */
+    var res = parseURL(location.href);
+    if (res) {
+        switch (res['tab']) {
+            case '2':
+                $('#sq').click();
+                break;
+            case '3':
+                $('#tj').click();
+                break;
+            default:
+                $('#dk').click();
+                break;
+        }
+    }
+    /**
      * 考勤按钮的时间显示
      */
     setInterval(function () {
@@ -9,17 +26,30 @@ $(document).ready(function () {
         var hours = dateObj.getHours();
         var minutes = dateObj.getMinutes();
         var seconds = dateObj.getSeconds();
-        if(hours<10){
-            hours = "0"+hours;
+        if (hours < 10) {
+            hours = "0" + hours;
         }
-        if(minutes<10){
-            minutes = "0"+minutes;
+        if (minutes < 10) {
+            minutes = "0" + minutes;
         }
-        if(seconds<10){
-            seconds = "0"+seconds;
+        if (seconds < 10) {
+            seconds = "0" + seconds;
         }
-        var newDate = month+"月"+date+"日"+"&nbsp"+hours+":"+minutes+":"+seconds;
+        var newDate = month + "月" + date + "日" + "&nbsp" + hours + ":" + minutes + ":" + seconds;
         $('#show-time').html(newDate);
     }, 1000);
     //picker-calendar-day
 });
+
+function parseURL(url) {
+    var url = url.split('?')[1];
+    if (url) {
+        var param = url.split('&');
+        var res = {};
+        param.forEach(function (p) {
+            arr = p.split('=');
+            res[arr[0]] = arr[1];
+        });
+        return res;
+    }
+}
