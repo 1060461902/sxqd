53ae0de4a154a814c21f0c121c785b3fa067b338
name <s>
2018-07-12 14:41:05 +0800

    7-12 14:41

--- assets/js/weixin_bundle.js
+++ assets/js/weixin_bundle.js
@@ -1,7 +1,11 @@
-function bundle() {
+function dobind() {
     if(true){
-        $.toptip('操作成功', 'success');
+        $.toptip('绑定成功', 'success');
     }else{
         $.toptip('绑定失败', 'error');
     }
+}
+
+function dounbind() {
+    $.confirm('确定要解绑吗？',function () {$.toptip('已解绑','success')},function () {$.toptip('已取消','warning')})
 }
