a6d1081290bf4d2188680120b9eea0c3fbf875bf
etoile_xd <984816138@qq.com>
2018-08-11 17:21:18 +0800

    8-11

--- assets/js/weixin_bundle.js
+++ assets/js/weixin_bundle.js
@@ -1,11 +1,35 @@
 function dobind() {
-    if(true){
-        $.toptip('绑定成功', 'success');
-    }else{
-        $.toptip('绑定失败', 'error');
+    var userid = $('#student-id-input').val();
+    var password = $('#student-pwd-input').val();
+    if (userid == null || userid == ''){
+        $.toptip('请输入学号', 'error');
+    }else if (password == null || password == ''){
+        $.toptip('请输入密码', 'error');
+    }else {
+        $.ajax({
+            type: 'POST',
+            url: '../bind',
+            data: {
+                'username':userid,
+                'password':password
+            },
+            success: function (d) {
+                if (d.code === 200){
+                    $.toptip('绑定成功', 'success');
+                }else {
+                    $.toptip('系统繁忙', 'error');
+                }
+            },
+            error: function (res) {
+                console.log(res);
+                $.toptip('系统繁忙', 'error');
+            }
+        });
     }
 }
 
 function dounbind() {
-    $.confirm('确定要解绑吗？',function () {$.toptip('已解绑','success')},function () {$.toptip('已取消','warning')})
+    $.confirm('确定要解绑吗？（功能暂未实现）',function () {
+        $.toptip('目前无法解绑','success')
+    },function () {$.toptip('已取消','warning')})
 }
