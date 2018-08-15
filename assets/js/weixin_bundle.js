ce28e05a38fb1c6112ed09591e6c489bc7875bb2
name <s>
2018-08-15 12:48:15 +0800

    815

--- assets/js/weixin_bundle.js
+++ assets/js/weixin_bundle.js
@@ -1,22 +1,43 @@
+$(document).ready(function () {
+    $.ajax({
+        type: 'GET',
+        url: '../auth/bind',
+        data: {},
+        success: function (d) {
+            if (d.code === 502) {
+                $.toptip('该微信已绑定账号', 'warning');
+            } else {
+                $.toptip('系统繁忙', 'error');
+            }
+        },
+        error: function (res) {
+            console.log(res);
+            $.toptip('系统繁忙', 'error');
+        }
+    });
+});
+
 function dobind() {
     var userid = $('#student-id-input').val();
     var password = $('#student-pwd-input').val();
-    if (userid == null || userid == ''){
+    if (userid == null || userid == '') {
         $.toptip('请输入学号', 'error');
-    }else if (password == null || password == ''){
+    } else if (password == null || password == '') {
         $.toptip('请输入密码', 'error');
-    }else {
+    } else {
         $.ajax({
             type: 'POST',
             url: '../bind',
             data: {
-                'username':userid,
-                'password':password
+                'username': userid,
+                'password': password
             },
             success: function (d) {
-                if (d.code === 200){
+                if (d.code === 200) {
                     $.toptip('绑定成功', 'success');
-                }else {
+                } else if (d.code === 501) {
+                    $.toptip('请检查账号密码', 'warning');
+                } else {
                     $.toptip('系统繁忙', 'error');
                 }
             },
@@ -29,7 +50,9 @@ function dobind() {
 }
 
 function dounbind() {
-    $.confirm('确定要解绑吗？（功能暂未实现）',function () {
-        $.toptip('目前无法解绑','success')
-    },function () {$.toptip('已取消','warning')})
+    $.confirm('确定要解绑吗？（功能暂未实现）', function () {
+        $.toptip('目前无法解绑', 'success')
+    }, function () {
+        $.toptip('已取消', 'warning')
+    })
 }
